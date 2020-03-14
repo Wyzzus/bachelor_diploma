d02bac7c6c60ca5583ab16d3314489928f8991d6
fominykhartur <fominykhartur+GitHub@gmail.com>
2020-03-14 23:19:32 +0300

    #6 Create MotorComponent. Cast to CECS

---
+++ Source/Assets/Scripts/DiceComponent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceComponent : MonoBehaviour
+{
+    public string Result;
+    public Text Field;
+
+    private void Update()
+    {
+        Field.text = Result;
+    }
+
+    public string GetResult()
+    {
+        return Result;
+    }
+
+    public void DiceRoll(int n)
+    {
+        Result = "Бросаем кубик ";
+        StopAllCoroutines();
+        StartCoroutine(DelayedRoll(n));
+    }
+
+    IEnumerator DelayedRoll(int n)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Result += ".";
+            yield return new WaitForSeconds(0.4F);
+        }
+        
+        Result = "Выпало " + Random.Range(1, n + 1);
+    }
+}
