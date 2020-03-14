f55504aa304a551e4cc70a46cd54d5b52037f2aa
fominykhartur <fominykhartur+GitHub@gmail.com>
2020-03-14 22:54:50 +0300

    #8 Create dice component

---
+++ Source/Assets/Scripts/DiceComponent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceComponent : MonoBehaviour
+{
+    public string Result;
+    public Text Field;
+
+    private void Update()
+    {
+        Field.text = Result;
+    }
+
+    public void DiceRoll(int n)
+    {
+        Result = "Бросаем кубик ";
+        StopAllCoroutines();
+        StartCoroutine(DelayedRoll(n));
+    }
+
+    IEnumerator DelayedRoll(int n)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Result += ".";
+            yield return new WaitForSeconds(0.4F);
+        }
+        
+        Result = "Выпало " + Random.Range(1, n + 1);
+    }
+}
