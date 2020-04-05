160b98c7106463b05e0153c2ad01d969f5c8c663
Yurij <vebvab@mail.ru>
2020-04-05 03:54:16 +0300

    Add room creating and joining handlers

---
+++ Source/Assets/LobbyManager.cs
@@ -0,0 +1,55 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    public Text LogText;
+    public InputField NickNameInput;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        PhotonNetwork.NickName = "Player " + Random.Range(1000, 9999);
+        Log("Player's name is set to " + PhotonNetwork.NickName);
+
+        PhotonNetwork.AutomaticallySyncScene = true; 
+        PhotonNetwork.GameVersion = "1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Log("Connected to Master");
+    }
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.NickName = NickNameInput.text;
+        PlayerPrefs.SetString("NickName", NickNameInput.text);
+        PhotonNetwork.CreateRoom(null, new Photon.Realtime.RoomOptions { MaxPlayers = 2 });
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.NickName = NickNameInput.text;
+        PlayerPrefs.SetString("NickName", NickNameInput.text);
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Log("Joined the room");
+
+        PhotonNetwork.LoadLevel("Game"); 
+    }
+
+    // Update is called once per frame
+    private void Log(string message)
+    {
+        Debug.Log(message);
+        LogText.text += "\n";
+        LogText.text += message;
+    }
+}
---
+++ Source/Assets/TestConnect.cs
@@ -0,0 +1,39 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+
+    private void Start()
+    {
+        Debug.Log("Connecting to Photon...", this);
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+        PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Photon.", this);
+
+        Debug.Log("My nickname is " + PhotonNetwork.LocalPlayer.NickName, this);
+        if (!PhotonNetwork.InLobby)
+            PhotonNetwork.JoinLobby();
+
+        //PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("Failed to connect to Photon: " + cause.ToString(), this);
+    }
+
+    public override void OnJoinedLobby()
+    {
+        print("Joined lobby");
+    }
+}
